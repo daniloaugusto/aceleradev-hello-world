0b88e91883385cef913aa9a6cec46cab46758211
daniloaugusto <dagrs70@gmail.com>
2019-06-09 22:04:30 -0300

    Adiciona o hello git

--- hello/hello.py
+++ hello/hello.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 
+def say_hello_git():
+    print('Hello, git')
+
+
 def say_hello_python():
     print('Hello, Python')
 
+
 def main():
     print("Hello, world!!")
-
+    say_hello_git()
     say_hello_python()
-
 
 
 if __name__ == "__main__":
